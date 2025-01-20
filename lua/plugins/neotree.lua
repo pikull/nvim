219e270a4709583dd74423dd8a9206d515e4e4a9
pikull <load.dot.ing@gmail.com>
2025-01-20 14:11:18 -0800

    edited remaps

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,7 +8,7 @@ return {
     },
     config = function()
         local opts = { noremap = true, silent = true }
-        vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle left<CR>", opts)
-        vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle float<CR>", opts)
+        vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle left<CR>", opts)
+        vim.keymap.set("n", "<leader>pv", ":Neotree filesystem toggle float<CR>", opts)
     end,
 }
