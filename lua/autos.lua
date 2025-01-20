39a95f8a4205c50d2caf2b51a747e1af60fc979b
pikull <load.dot.ing@gmail.com>
2025-01-20 14:24:55 -0800

    added format on save

--- lua/autos.lua
+++ lua/autos.lua
@@ -1,13 +1,7 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
-    group = vim.api.nvim_create_augroup("AutoIndent", {}),
-    pattern = "*",
-    command = "normal! gg=G",
-})
-
 vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("HighlightYank", {}),
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
-    end
+	group = vim.api.nvim_create_augroup("HighlightYank", {}),
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
+	end,
 })
