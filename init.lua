b64c3004ee4028cc992622c87f63bc596ff1200c
pikull <load.dot.ing@gmail.com>
2025-01-19 21:45:34 -0800

    reorganized and added highlight on yank

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
 require("options")
 require("remaps")
-require("autos")
 
 require("lazy_init")
 require("lazy").setup("plugins", {
     install = { colorscheme = { "catppuccin" } },
     checker = { enabled = true },
 })
+
+require("autos")
--- lua/autos.lua
+++ lua/autos.lua
@@ -1,4 +1,25 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
+vim.api.nvim_create_autocmd({ "BufWritePre", "VimLeave" }, {
+    group = vim.api.nvim_create_augroup("AutoIndent", {}),
     pattern = "*",
     command = "normal! gg=G",
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("HighlightYank", {}),
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
+    end
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("LspConfig", {}),
+    callback = function(ev)
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set({'n', 'v'}, "<leader>ca", vim.lsp.buf.code_action, opts)
+    end
 })
