1f9271e228109a016c4b9cd6b7ab322c2d0b93a0
pikull <load.dot.ing@gmail.com>
2025-01-22 13:27:19 -0800

    don't show neotree except instead of netrw

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,37 +1,34 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		local opts = { noremap = true, silent = true }
-		vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle left<CR>", opts)
-		vim.keymap.set("n", "<leader>pv", ":Neotree filesystem toggle float<CR>", opts)
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        local opts = { noremap = true, silent = true }
+        vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle left<CR>", opts)
+        vim.keymap.set("n", "<leader>pv", ":Neotree filesystem toggle float<CR>", opts)
 
-		local augroup = vim.api.nvim_create_augroup("NeoTreeInit", {})
-		vim.api.nvim_create_autocmd("UiEnter", {
-			group = augroup,
-			callback = function()
-				local f = vim.fn.expand("%:p")
-				if vim.fn.isdirectory(f) ~= 0 then
-					vim.cmd("Neotree current dir=" .. f)
-					vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
-				else
-					vim.cmd("Neotree filesystem show left")
-					vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
-				end
-			end,
-		})
+        local augroup = vim.api.nvim_create_augroup("NeoTreeInit", {})
+        vim.api.nvim_create_autocmd("UiEnter", {
+            group = augroup,
+            callback = function()
+                local f = vim.fn.expand("%:p")
+                if vim.fn.isdirectory(f) ~= 0 then
+                    vim.cmd("Neotree current dir=" .. f)
+                    vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
+                end
+            end,
+        })
 
-		vim.g.loaded_netrwPlugin = 1
-		vim.g.loaded_netrw = 1
-	end,
-	opts = {
-		filesystem = {
-			hijack_netrw_behavior = "open_current",
-		},
-	},
+        vim.g.loaded_netrwPlugin = 1
+        vim.g.loaded_netrw = 1
+    end,
+    opts = {
+        filesystem = {
+            hijack_netrw_behavior = "open_current",
+        },
+    },
 }
