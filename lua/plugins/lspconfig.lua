6de8618a590d47bc554774605034d8ab7a764f3d
pikull <load.dot.ing@gmail.com>
2025-01-27 20:13:30 -0800

    added lazydev.nvim

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,47 +1,60 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                -- lsp setup
-                ensure_installed = { "lua_ls", "pyright", "clangd", "html", "cssls", "ts_ls" },
-                -- end setup
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local cp = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				-- lsp setup
+				ensure_installed = { "lua_ls", "pyright", "clangd", "html", "cssls", "ts_ls" },
+				-- end setup
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			local cp = require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
-            local lsp_opts = { capabilities = cp }
-            -- lsp setup
-            lspconfig.lua_ls.setup(lsp_opts)
-            lspconfig.pyright.setup(lsp_opts)
-            lspconfig.clangd.setup(lsp_opts)
-            lspconfig.html.setup(lsp_opts)
-            lspconfig.cssls.setup(lsp_opts)
-            lspconfig.ts_ls.setup(lsp_opts)
-            -- end setup
+			local lspconfig = require("lspconfig")
+			local lsp_opts = { capabilities = cp }
+			-- lsp setup
+			lspconfig.lua_ls.setup(lsp_opts)
+			lspconfig.pyright.setup(lsp_opts)
+			lspconfig.clangd.setup(lsp_opts)
+			lspconfig.html.setup(lsp_opts)
+			lspconfig.cssls.setup(lsp_opts)
+			lspconfig.ts_ls.setup(lsp_opts)
+			-- end setup
 
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("LspConfig", {}),
-                callback = function(ev)
-                    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("LspConfig", {}),
+				callback = function(ev)
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-                    local opts = { buffer = ev.buf, noremap = true, silent = true }
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-                end,
-            })
-        end,
-    },
+					local opts = { buffer = ev.buf, noremap = true, silent = true }
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+				end,
+			})
+		end,
+	},
 }
